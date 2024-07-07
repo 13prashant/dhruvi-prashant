74c35ff33f3cbe126e3d8d9d8c20a0206ff0d213
13prashant <pacific1392@gmail.com>
2024-07-07 20:58:47 +0530

    added google calendar event for non-safari/iOs devices

--- components/SaveToCalendarButton.jsx
+++ components/SaveToCalendarButton.jsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { checkDevice } from "@/lib/checkDevice";
+import { getBrowserName } from "@/lib/getBrowserName";
 import { generateICS } from "@/lib/icsGenerator";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
+const location = "Ramji Vadi, Budiya, Surat- 395007";
 const googleMapsUrl = "https://maps.app.goo.gl/nrmQMFvFXswUfwQb7";
 const organizerPhoneNumber = "+91 9662017916";
 
@@ -11,7 +15,7 @@ const event = {
   duration: { hours: 4, minutes: 0 },
   title: "Dhruvi & Prashant's Wedding",
   description: `Join us for the wedding ceremony of Dhruvi & Prashant.\n\nLocation: ${googleMapsUrl}\nPhone: ${organizerPhoneNumber}`,
-  location: "Ramji Vadi, Budiya, Surat- 395007",
+  location,
   url: "https://dhruvi.prashaant.in",
   status: "CONFIRMED",
   busyStatus: "BUSY",
@@ -24,7 +28,23 @@ const event = {
   ],
 };
 
+const googleCalendarUrl = `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(
+  event.title
+)}&details=${encodeURIComponent(
+  event.description
+)}&location=${encodeURIComponent(
+  location
+)}&dates=20240721T093000Z/20240721T150000Z&ctz=Asia/Kolkata`;
+
 export default function SaveToCalendarButton() {
+  const [device, setDevice] = useState("other");
+  const [browser, setBrowser] = useState("");
+
+  useEffect(() => {
+    setDevice(checkDevice());
+    setBrowser(getBrowserName());
+  }, []);
+
   const handleSaveToCalendar = async () => {
     try {
       const icsValue = await generateICS(event);
@@ -58,11 +78,24 @@ export default function SaveToCalendarButton() {
   };
 
   return (
-    <button
-      className="bg-primary/90 px-6 py-2 rounded-sm font-semibold cursor-pointer hover:bg-secondary text-primary-foreground duration-200"
-      onClick={handleSaveToCalendar}
-    >
-      Save to Calendar
-    </button>
+    <>
+      {device === "iOs" && browser === "Safari" ? (
+        <button
+          className="bg-primary/90 px-6 py-2 rounded-sm font-semibold cursor-pointer hover:bg-secondary text-primary-foreground duration-200"
+          onClick={handleSaveToCalendar}
+        >
+          Save to Calendar
+        </button>
+      ) : (
+        <a
+          href={googleCalendarUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-primary/90 px-6 py-2 rounded-sm font-semibold cursor-pointer hover:bg-secondary text-primary-foreground duration-200"
+        >
+          Save to Calendar
+        </a>
+      )}
+    </>
   );
 }
