8be0c1a4c9ed6a71a36d96dca2f9a17c51b2e42c
13prashant <pacific1392@gmail.com>
2024-08-29 12:01:34 +0530

    fixed clarity-script

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,13 +50,11 @@ export default function RootLayout({ children }) {
           toastOptions={{}}
         />
         <Script id="clarity-script" strategy="afterInteractive">
-          <script type="text/javascript">
-            {`(function(c,l,a,r,i,t,y){
+          {`(function(c,l,a,r,i,t,y){
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
               })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");`}
-          </script>
         </Script>
       </body>
     </html>
